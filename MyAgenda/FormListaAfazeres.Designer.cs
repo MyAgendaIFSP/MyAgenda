117820b24a4cd843754f411181e33ff234515158
Leticia Sayuri <leticiasyrk@gmail.com>
2017-10-21 18:57:04 -0200

    Criação do form de Lista de Afazeres e inclusão da barra de navegação no mesmo.

---
+++ MyAgenda/FormListaAfazeres.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MyAgenda {
+    partial class FormListaAfazeres {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.barraNavegacao1 = new MyAgenda.Componentes.BarraNavegacao();
+            this.SuspendLayout();
+            // 
+            // barraNavegacao1
+            // 
+            this.barraNavegacao1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.barraNavegacao1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.barraNavegacao1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barraNavegacao1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.barraNavegacao1.Location = new System.Drawing.Point(0, 0);
+            this.barraNavegacao1.Name = "barraNavegacao1";
+            this.barraNavegacao1.Size = new System.Drawing.Size(827, 40);
+            this.barraNavegacao1.TabIndex = 0;
+            // 
+            // FormListaAfazeres
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 473);
+            this.Controls.Add(this.barraNavegacao1);
+            this.Name = "FormListaAfazeres";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyAgenda | Lista de Afazeres";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Componentes.BarraNavegacao barraNavegacao1;
+    }
+}
---
+++ MyAgenda/FormListaAfazeres.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static MyAgenda.Componentes.BarraNavegacao;
+
+namespace MyAgenda {
+    public partial class FormListaAfazeres : Form {
+        public FormListaAfazeres() {
+            InitializeComponent();
+            barraNavegacao1.MenuItemClick += _click;
+
+            barraNavegacao1.AddItem("Início", Properties.Resources.ic_home_white, 1);
+            barraNavegacao1.AddItem("Pomodoro", 2);
+            barraNavegacao1.AddItem("Tarefas", 3);
+            barraNavegacao1.AddItem("Sair", Properties.Resources.ic_exit_to_app_white, EPosicao.DIREITA, 4);
+            barraNavegacao1.AddItem("Offline", Properties.Resources.ic_cloud_queue_white, EPosicao.DIREITA, _cloudClick, 5);
+        }
+
+        private void _cloudClick(ref Button btn, int itemId) {
+            MessageBox.Show("botão Nuvem");
+
+            btn.Image = Properties.Resources.ic_cloud_white;
+        }
+
+        private void _click(ref Button btn, int itemId) {
+            switch(itemId) {
+                case 1:
+                    MessageBox.Show("botão início");
+                    break;
+                case 2:
+                    MessageBox.Show("botão Pomodoro");
+                    break;
+                case 3:
+                    MessageBox.Show("botão Tarefas");
+                    break;
+                case 4: MessageBox.Show("botão sair");
+                    break;
+            }
+        }
+    }
+}
