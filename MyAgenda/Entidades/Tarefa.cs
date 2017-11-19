22be209fa095ec45cce120fb149689e4ef9bb7d2
Leticia Sayuri <leticiasyrk@gmail.com>
2017-11-18 22:39:13 -0200

    Criação das entidades Evento, ListaAfazeres, Tarefa e Usuario.

---
+++ MyAgenda/Entidades/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAgenda.Entidades
+{
+    public class Tarefa
+    {
+        public ListaAfazeres Lista;
+
+        public string Titulo;
+
+        public DateTime Data;
+
+        public string Descricao;
+
+        public Usuario Usuario;
+    }
+}
---
+++ MyAgenda/Entidades/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAgenda.Entidades
+{
+    public class Usuario
+    {
+        public long IdUsuario;
+
+        public string Nome;
+
+        public string Email;
+
+        public string Senha;
+
+        public DateTime DataNascimento;
+    }
+}
