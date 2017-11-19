af0c1064662ca998681d9b25224b9a411fc1a7c8
Leticia Sayuri <leticiasyrk@gmail.com>
2017-11-19 00:14:20 -0200

    Reinclusão do componente ItemTarefa e criação do form de lista de afazeres e criação de nova lista.

---
+++ MyAgenda/FormListaAfazeres.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using MyAgenda.Componentes;
+using MyAgenda.Entidades;
+using MyAgenda.Dados;
+
+namespace MyAgenda
+{
+    public partial class FormListaAfazeres : Form
+    {
+
+        private CultureInfo cultureInfo = new CultureInfo("pt-BR");
+
+        public FormListaAfazeres()
+        {
+            InitializeComponent();
+        }
+
+        private void _populaComboBoxDeListasdeAfazeres()
+        {
+            ListaAfazeresAPI listaAfazeresAPI = new ListaAfazeresAPI();
+
+            List<ListaAfazeres> listasAfazeres = new List<ListaAfazeres>();
+            listasAfazeres = listaAfazeresAPI.CarregaListasAfazeres();
+
+            foreach (ListaAfazeres lista in listasAfazeres)
+            {
+                cbbListasAfazeres.Items.Add(lista.Titulo);
+            }
+        }
+
+        private void FormListaAfazeres_Load(object sender, EventArgs e)
+        {
+            _populaComboBoxDeListasdeAfazeres();
+
+            _carregaTarefasDaBaseDeDados();
+        }
+
+        private void btnNovaLista_Click(object sender, EventArgs e)
+        {
+            FormNovaListaAfazeres form = new FormNovaListaAfazeres();
+            form.Show();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            Tarefa tarefa = new Tarefa();
+            tarefa.Titulo = "Teste";
+            tarefa.Descricao = txtTarefa.Text;
+            tarefa.Data = cldData.SelectionRange.Start;
+
+            Usuario usuario = new Usuario();
+            usuario.IdUsuario = 1;
+
+            ListaAfazeres lista = new ListaAfazeres();
+            lista.Titulo = "Tarefas da Faculdade";
+
+            tarefa.Lista = lista;
+
+            TarefaAPI tarefaAPI = new TarefaAPI();
+            tarefaAPI.AdicionaTarefa(tarefa);
+            _carregaTarefasDaBaseDeDados();
+        }
+
+        private void _carregaTarefasDaBaseDeDados()
+        {
+            TarefaAPI tarefaAPI = new TarefaAPI();
+            List<Tarefa> tarefas = tarefaAPI.CarregaTarefas("Tarefas da Faculdade");
+
+            int y = 0;
+            foreach (Tarefa tarefa in tarefas)
+            {
+                ItemTarefa itemTarefa = new ItemTarefa(tarefa);
+                itemTarefa.DescricaoTarefa = tarefa.Descricao;
+                itemTarefa.Width = pnlListaAfazeres.Width - 20;
+                itemTarefa.Location = new Point(0, y);
+                pnlListaAfazeres.Controls.Add(itemTarefa);
+                y = pnlListaAfazeres.Controls[pnlListaAfazeres.Controls.Count - 1].Bottom + 10;
+
+            }
+        }
+    }
+}
---
+++ MyAgenda/FormNovaListaAfazeres.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyAgenda.Dados;
+using MyAgenda.Entidades;
+
+namespace MyAgenda
+{
+    public partial class FormNovaListaAfazeres : Form
+    {
+        public FormNovaListaAfazeres()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNovaLista_Click(object sender, EventArgs e)
+        {
+            ListaAfazeres listaAfazeres = new ListaAfazeres();
+            listaAfazeres.Titulo = txtTitulo.Text;
+            listaAfazeres.DataCriacao = DateTime.Now;
+
+            Usuario usuario = new Usuario();
+            usuario.IdUsuario = 1;
+
+            listaAfazeres.Usuario = usuario;
+
+            ListaAfazeresAPI listaAfazeresAPI = new ListaAfazeresAPI();
+            listaAfazeresAPI.AdicionaLista(listaAfazeres);
+            this.Close();
+        }
+    }
+}
