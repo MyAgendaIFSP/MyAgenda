b561e39b8e71076642701da6471c8221e9186c10
Leticia Sayuri <leticiasyrk@gmail.com>
2017-11-25 22:48:21 -0200

    Criação do form de alerta de eventos próximos.

---
+++ MyAgenda/FormAlertaEvento.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyAgenda.Entidades;
+
+namespace MyAgenda
+{
+    public partial class FormAlertaEvento : Form
+    {
+
+        public Evento Evento;
+
+        public FormAlertaEvento()
+        {
+            InitializeComponent();
+        }
+
+        public FormAlertaEvento(Evento evento, double minutos)
+        {
+            InitializeComponent();
+
+            Evento = evento;
+
+            string alerta = string.Format("O evento {0} começará em {1} minutos", Evento.Titutlo, minutos); 
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
